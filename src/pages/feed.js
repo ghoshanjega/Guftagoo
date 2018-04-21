9b1af6a17d1ff79a10fe1b5b76e259e946360ec1
ghoshanjega <gjaa@connect.ust.hk>
2018-04-22 02:51:23 +0800

    done feed. except kshitij has to fix the image

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -11,9 +11,9 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {List, ListItem} from 'material-ui/List';
 import FontIcon from 'material-ui/FontIcon';
 import Avatar from 'material-ui/Avatar';
-import {Card, CardActions, CardHeader, CardText, CardTitle} from 'material-ui/Card';
+import {Card, CardActions, CardHeader, CardText, CardTitle, } from 'material-ui/Card';
 import LinearProgress from 'material-ui/LinearProgress';
-import { AccessAlarm, ThreeDRotation, Book } from 'material-ui-icons';
+import { AccessAlarm, ThreeDRotation, Book, FormatBold, FormatColorFill, CloudCircle, Computer, Smartphone, Visibility, Lens } from 'material-ui-icons';
 import Chip from 'material-ui/Chip';
 
 
@@ -39,44 +39,83 @@ const style = {
   margin: 20,
   textAlign: 'center',
   display: 'inline-block',
-  border : "20px"
+//   border : "20px"
 };
 
 const Feed = () => (
         <MuiThemeProvider muiTheme={ getMuiTheme(baseTheme, overwrites)}>
         <List style={{justifyContent:"center"}}>
-            <ListItem>
-        <Paper style={style} zDepth={5} >
+            <ListItem style={{left:"-15"}}>
+        <Paper style={style} zDepth={3}  >
         <div style={{display:"flex", margin: "auto", position: "relative",
 top: "50%",
-transform: "translateY(-50%)"}}>
+transform: "translateY(-90%)"}}>
                 <div style={{flex: "50%"}}>
                 <img src="/src/pages/daniel.jpg"/></div>
                 <div style={{flex: "50%"}}>
                 
-                <h2 style={{lineHeight:"1.0"}}>Ghoshan Jaganathamani</h2>
+                <h2 style={{lineHeight:"1.0"}}>Boruto Morty</h2>
                 <List>
                 <ListItem disabled={true} leftAvatar={<Avatar> <Book/> </Avatar>}>Computer Science</ListItem>
-                <ListItem disabled={true} leftAvatar={<Avatar> CS </Avatar>}>Computer Science</ListItem>
+                <ListItem disabled={true} leftAvatar={<Avatar> SL </Avatar>}>Sri Lankan</ListItem>
                 </List>
                 </div>
         </div>
+        <br />
+        <br />
+        
+        
+
+        <div style={{display: "flex",
+    flexWrap: "wrap", fontSize:"8", color:"#00BFA5"}}>
+        <Chip backgroundColor={"#00BFA5"} style={{marginLeft:"10",marginBottom:"10"}}>
+          <Avatar icon={<AccessAlarm /> } />
+          Relativity
+        </Chip>
+
+        <Chip backgroundColor={"#00BFA5"} style={{marginLeft:"10",marginBottom:"10"}}>
+          <Avatar icon={<FormatBold /> } />
+          BitCoin
+        </Chip>
+
+        <Chip backgroundColor={"#00BFA5"} style={{marginLeft:"10",marginBottom:"10"}}>
+          <Avatar icon={<FormatColorFill /> } />
+          Chemistry
+        </Chip>
+
+        <Chip backgroundColor={"#00BFA5"} style={{marginLeft:"10",marginBottom:"10"}}>
+          <Avatar icon={<CloudCircle /> } />
+          Cloud Computing
+        </Chip>
+
+        <Chip backgroundColor={"#00BFA5"} style={{marginLeft:"10",marginBottom:"10"}}>
+          <Avatar icon={<Computer  /> } />
+          Computers
+        </Chip>
+
+        <Chip backgroundColor={"#00BFA5"} style={{marginLeft:"10",marginBottom:"10"}}>
+          <Avatar icon={<Smartphone /> } />
+          Phones
+        </Chip>
+        </div> 
         </Paper>
         </ListItem>
-
-         <ListItem>
+        <ListItem>
   <Card 
   containerStyle={{backgroundColor:"#ffffff"}}>
     <CardTitle 
      //style={{height: "80px"}}
-     titleStyle={{ fontSize: '30px' }}
-     title="Is sexual orientation determined at birth?"
+     titleStyle={{ fontSize: '25px' }}
+     title="Microsoft should invest in a contingency plan for singularity?"
      titleColor="#123456" >
     </CardTitle>
-    <CardText style={{float:"right"}}> PSYCHOLOGICAL
-    </CardText> 
-    <LinearProgress mode="determinate" value={40} style={{height: "10"}} color={"#9CCC65"}/>
-    
+    {/* <CardText > */}
+    <div style={{float:"left", marginBottom:"10", marginLeft:"15", color:"#FF0000",display:"flex"}}><Lens style={{marginRight:"10", size: "3px"}} color={"#FF0000"}/> ON GOING </div>
+   
+        <div style={{float:"right", marginBottom:"10", marginRight:"15"}}> POLITICAL </div>
+        
+    {/* </CardText>  */}
+    <LinearProgress mode="determinate" value={70} style={{height: "10"}} color={"#9CCC65"} />
   </Card>
   </ListItem>
 
@@ -85,17 +124,79 @@ transform: "translateY(-50%)"}}>
   containerStyle={{backgroundColor:"#ffffff"}}>
     <CardTitle 
      //style={{height: "80px"}}
-     titleStyle={{ fontSize: '30px' }}
+     titleStyle={{ fontSize: '25px' }}
      title="Should adults have the right to carry a concealed handgun?"
      titleColor="#123456" >
     </CardTitle>
     {/* <CardText > */}
     <div style={{float:"left", marginBottom:"10", marginLeft:"15"}}> 123,434 views </div>
+   
         <div style={{float:"right", marginBottom:"10", marginRight:"15"}}> POLITICAL </div>
+        
     {/* </CardText>  */}
     <LinearProgress mode="determinate" value={70} style={{height: "10"}} color={"#9CCC65"} />
   </Card>
   </ListItem>
+
+    <ListItem>
+  <Card 
+  containerStyle={{backgroundColor:"#ffffff"}}>
+    <CardTitle 
+     //style={{height: "80px"}}
+     titleStyle={{ fontSize: '25px' }}
+     title="Are we there yet"
+     titleColor="#123456" >
+    </CardTitle>
+    {/* <CardText > */}
+    <div style={{float:"left", marginBottom:"10", marginLeft:"15"}}> 123,434 views </div>
+   
+        <div style={{float:"right", marginBottom:"10", marginRight:"15"}}> GENERAL </div>
+        
+    {/* </CardText>  */}
+    <LinearProgress mode="determinate" value={80} style={{height: "10"}} color={"#9CCC65"} />
+  </Card>
+  </ListItem>
+
+      <ListItem>
+  <Card 
+  containerStyle={{backgroundColor:"#ffffff"}}>
+    <CardTitle 
+     //style={{height: "80px"}}
+     titleStyle={{ fontSize: '25px' }}
+     title="Should pedestrians walk on left?"
+     titleColor="#123456" >
+    </CardTitle>
+    {/* <CardText > */}
+    <div style={{float:"left", marginBottom:"10", marginLeft:"15"}}> 123,434 views </div>
+   
+        <div style={{float:"right", marginBottom:"10", marginRight:"15"}}> GENERAL </div>
+        
+    {/* </CardText>  */}
+    <LinearProgress mode="determinate" value={30} style={{height: "10"}} color={"#9CCC65"} />
+  </Card>
+  </ListItem>
+
+    <ListItem style={{marginBottom:"50"}}>
+  <Card 
+  containerStyle={{backgroundColor:"#ffffff"}}>
+    <CardTitle 
+     //style={{height: "80px"}}
+     titleStyle={{ fontSize: '25px' }}
+     title="Is terrorism and religion unseperatable?"
+     titleColor="#123456" >
+    </CardTitle>
+    {/* <CardText > */}
+    <div style={{float:"left", marginBottom:"10", marginLeft:"15"}}> 123,434 views </div>
+   
+        <div style={{float:"right", marginBottom:"10", marginRight:"15"}}> RELIGION </div>
+        
+    {/* </CardText>  */}
+    <LinearProgress mode="determinate" value={70} style={{height: "10"}} color={"#9CCC65"} />
+  </Card>
+  </ListItem>
+
+
+
 
     </List>
     </MuiThemeProvider>
